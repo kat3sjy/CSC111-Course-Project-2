f5387340d938eeee8f1a6c1331a6917de147333c
404SiriNotFound <sarakopilovic@gmail.com>
2025-03-26 23:54:29 -0400

    i hope you see this :)
    data wrangling

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 
-# hellooo
+from __future__ import annotations
+import csv
+from typing import Any
+
+columns_to_keep = [
+    'track_id', 'artists', 'popularity', 'explicit', 'danceability', 'energy',
+    'speechiness', 'acousticness', 'instrumentalness', 'liveness',
+    'valence', 'tempo', 'track_genre'
+]
+
+with open('spotify_songs.csv', 'r') as file:
+    reader = csv.reader(file)
+    filtered_rows = []
+    for row in reader:
+        filter_row = {song: row[song] for song in columns_to_keep}
+        filtered_rows.append(filter_row)
 
 
-# this is a change fadsfasfd
-
-# kristen >:3
-#heyyy its Sara
-
-# cindy :)
-
-print("hello world")
