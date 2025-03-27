8070a693cc40ca660ef6fb9647529360a5dcbc2d
kate <kateshen2006@gmail.com>
2025-03-27 13:15:51 -0400

    random features

--- visualization.py
+++ visualization.py
@@ -11,8 +11,15 @@ title_x = 750
 title_y = 150
 clock = pygame.time.Clock()
 running = True
+user_input_active = False
+user_input_text = ''
+question_index = 0
+question_list = ["Question 1", "Question 2", "Question 3"]
+
+# different fonts
 TITLE_FONT = pygame.font.Font("Fonts/Audiowide/Audiowide-Regular.ttf", 90)
 SUBTITLE_FONT = pygame.font.Font("Fonts/Audiowide/Audiowide-Regular.ttf", 60)
+PARAGRAPH_FONT = pygame.font.Font("Fonts/Audiowide/Audiowide-Regular.ttf", 25)
 
 while running:
     # poll for events
@@ -24,15 +31,29 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if window_x // 2 - title_x/3 <= mouse[0] <= window_x // 2 - title_x/3 + 500 and window_y // 2 <= mouse[1] <= window_y // 2 + 100:
+            if start_button.collidepoint(event.pos) and current == "home":
                 current = "recommender"
+            elif user_input.collidepoint(event.pos) and current == "recommender":
+                user_input_active = True
+            elif input_enter.collidepoint(event.pos) and current == "recommender":
+                user_input_text = ""
+                if question_index < len(question_list) - 1:
+                    question_index += 1
+                else:
+                    current = "recommendations"
+
+        elif event.type == pygame.KEYDOWN:
+                if user_input_active:
+                    if event.key == pygame.K_BACKSPACE:
+                        user_input_text = user_input_text[:-1]
+                    else:
+                        user_input_text += event.unicode
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
     if current == "home":
-        # RENDER YOUR GAME HERE
-
+        # title stuff
         title = TITLE_FONT.render("Spotify Recommender System", True, (255, 255, 255))
         screen.blit(title, (window_x // 2 - title_x, window_y // 2 - title_y))
         start_button = pygame.draw.rect(screen, (44, 201, 76), (window_x // 2 - title_x/3, window_y // 2, 500, 100), 0)
@@ -40,7 +61,21 @@ while running:
         screen.blit(start_button_text, (window_x // 2 - title_x/6, window_y // 2 +title_y/13))
 
     elif current == "recommender":
-        idk_button = pygame.draw.rect(screen, (44, 201, 76), (window_x // 2 - title_x/3, window_y // 2, 500, 100), 0)
+
+        question_text = SUBTITLE_FONT.render(question_list[question_index], True, (255,255,255))
+        screen.blit(question_text, (window_x // 2 - title_x/6, 200))
+
+        user_input = pygame.draw.rect(screen, (44, 201, 76), (window_x // 2 - title_x/3, window_y // 2, 500, 100), 0)
+        input_text = PARAGRAPH_FONT.render(user_input_text, True, (255,255,255))
+        screen.blit(input_text, (window_x // 2 - title_x/6, window_y // 2 +title_y/13))
+
+        input_enter = pygame.draw.rect(screen, (255,255,255), (window_x // 2 - title_x/3, window_y // 2 + 200, 500, 100), 0)
+        enter_text = SUBTITLE_FONT.render("ENTER", True, (0,0,0))
+        screen.blit(enter_text, (window_x // 2 - title_x/6, window_y // 2 +title_y/13+200))
+
+    elif current == "recommendations":
+        question_text = SUBTITLE_FONT.render("insert random recs", True, (255, 255, 255))
+        screen.blit(question_text, (window_x // 2 - title_x / 6, 200))
 
 
     # flip() the display to put your work on screen
